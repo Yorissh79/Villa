918fb22ed686185f0c9736e769981894e9d931e9
Yorissh79 <yusifbsh-azmp203@code.edu.az>
2025-05-12 09:04:36 +0400

    Filter & search

--- frontend/src/components/backcard/BackCard.jsx
+++ frontend/src/components/backcard/BackCard.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from './BackCard.module.scss'
 import Card from "./card/Card.jsx";
 
 const BackCard = ({data, who}) => {
+
+    const [searchText, setSearchText] = useState("")
+    const [filterText, setFilterText] = useState("0")
+
+    const filteredData = data?.filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()))
+        .sort((a, b) => {
+            if (filterText === "asc") return Number(a.price) - Number(b.price)
+            if (filterText === "desc") return Number(b.price) - Number(a.price)
+            if (filterText === "0") return 0
+        })
+
     return (
        <div className={style.main}>
-           {data?.map((item) => <Card item={item} who={who}/>)}
+           <div className={style.top}>
+               <input placeholder={"Seacrh"} onChange={(e) => setSearchText(e.target.value)} />
+               <div className={style.btn}>
+                   <button onClick={() => setFilterText("asc")}>Az</button>
+                   <button onClick={() => setFilterText("desc")}>Cox</button>
+                   <button onClick={() => setFilterText("0")}>Default</button>
+               </div>
+           </div>
+
+           <div className={style.cards}>
+               {filteredData?.map((item) => <Card item={item} who={who}/>)}
+           </div>
        </div>
     )
 }
